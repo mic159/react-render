98f01ea723e8a1d15f689363487b87b71c593da5
Michael Cooper <mic159@gmail.com>
2016-02-23 14:41:14 +1100

    Support manifest staticfiles storage.
    Find the hashed name of the file from the staticfiles_storage
    class.

--- react_render/django/render.py
+++ react_render/django/render.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 import os
 import logging
+from django.contrib.staticfiles.storage import staticfiles_storage, HashedFilesMixin
 from django.contrib.staticfiles.finders import find as find_static
 from django.core.serializers.json import DjangoJSONEncoder
 from django.utils.safestring import mark_safe
@@ -36,6 +37,15 @@ class RenderedComponent(object):
 
 def render_component(path_to_source, props=None, to_static_markup=False, json_encoder=None):
     if not os.path.isabs(path_to_source):
+        # If its using the manifest staticfiles storage, to the hashed name.
+        # eg. js/hello.js -> js/hello.d0bf07ff5f07.js
+        if isinstance(staticfiles_storage, HashedFilesMixin):
+            try:
+                path_to_source = staticfiles_storage.stored_name(path_to_source)
+            except ValueError:
+                # Couldn't find it.
+                pass
+        # Now resolve it to the absolute path using the finders
         path_to_source = find_static(path_to_source) or path_to_source
 
     if json_encoder is None:
