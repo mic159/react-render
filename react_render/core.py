3bfbf57fdd45ff4340e564c4ab4f7fb65d2d5e5d
Michael Cooper <mic159@gmail.com>
2020-08-13 09:04:34 +1000

    Use a thread local to try to keepalive

--- react_render/core.py
+++ react_render/core.py
@@ -1,9 +1,25 @@
 import os
 import json
+import threading
+
 import requests
 from react_render.exceptions import ComponentSourceFileNotFound, ComponentRenderingError
 
 DEFAULT_SERVICE_URL = 'http://localhost:63578/render'
+
+_requests_session_store = threading.local()
+
+
+def requests_keepalive_session(name) -> requests.Session:
+    """
+    Creates a session that can utilize keep-alive behaviour across multiple
+    requests, by maintaining the session within a thread-local.
+    """
+    session = getattr(_requests_session_store, name, None)
+    if not session:
+        session = requests.Session()
+        setattr(_requests_session_store, name, session)
+    return session
 
 
 def render_component(path_to_source, props=None, to_static_markup=False, json_encoder=None, service_url=None, timeout=10):
@@ -16,7 +32,8 @@ def render_component(path_to_source, props=None, to_static_markup=False, json_en
     if service_url is None:
         service_url = DEFAULT_SERVICE_URL
 
-    response = requests.post(service_url,
+    session = requests_keepalive_session()
+    response = session.post(service_url,
         timeout=timeout,
         headers={'Content-Type': 'application/json'},
         data=json_encoder({
